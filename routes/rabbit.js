16bb46a2718552e9d6e7c5a72623351aeeb43014
saipriyajetti <jsaipriyads@gmail.com>
2023-11-15 22:28:17 -0600

    files

--- routes/rabbit.js
+++ routes/rabbit.js
@@ -9,4 +9,12 @@ const rabbit_controlers= require('../controllers/rabbit');
 var router = express.Router();
 /* GET rabbits */
 router.get('/', rabbit_controlers.rabbit_view_all_Page );
+router.get('/detail', rabbit_controlers.rabbit_view_one_Page);
+router.get('/create', rabbit_controlers.rabbit_create_Page);
+/* GET create update page */
+router.get('/update', rabbit_controlers.rabbit_update_Page);
+/* GET delete rabbit page */
+router.get('/delete', rabbit_controlers.rabbit_delete_Page);
+
+
 module.exports = router;
