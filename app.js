7ec720b11bcd5da043af636103b8fed3bb040ec3
saipriyajetti <jsaipriyads@gmail.com>
2023-11-17 02:53:38 -0600

    files

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
