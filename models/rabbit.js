1228a27faadff4579d0b7296b82020748eddd5bd
saipriyajetti <jsaipriyads@gmail.com>
2023-11-29 16:19:29 -0600

    commit

--- models/rabbit.js
+++ models/rabbit.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const rabbitSchema = mongoose.Schema({
 rabbit_color: String,
-rabbit_breed: String,
-rabbit_price: Number
+rabbit_breed: {type:String,length:15},
+rabbit_price: {type:Number,min:1000,max:400000}
 })
 module.exports = mongoose.model("rabbit",rabbitSchema);
 
